fbcbd8aebaa289311f0b74201f0f58d38ed105d7
Mary Omange <omange.seun@gmail.com>
2021-07-06 09:45:51 -0700

    added

--- Investment/urls.py
+++ Investment/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('<int:pk>/', InvestmentGetDetailView.as_view(), name='Investment-details'),
     path('update/<int:pk>/', InvestmentUpdateView.as_view(), name='update_Investment'),
     path('delete/<int:pk>/', DeleteInvestmentView.as_view(), name='Delete_Investment'),
-    path('all_Investments/', AllInvestment.as_view(), name ='allInvestments')
+    path('all_Investment/', AllInvestment.as_view(), name ='allInvestment')
 ]
