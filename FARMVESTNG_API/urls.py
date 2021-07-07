b3c938202446b1a460092e1676f8d42a7aae3f57
JeanEric <ericjohn415@gmail.com>
2021-07-08 00:05:13 +0200

    urls.py file

--- FARMVESTNG_API/urls.py
+++ FARMVESTNG_API/urls.py
@@ -1,18 +1,3 @@
-"""FARMVESTNG_API URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -42,7 +27,7 @@ urlpatterns = [
     path("api/v1/", include("Investment.urls")),
     path("api/v1/", include("Referral.urls")),
     path(
-        "documentation",
+        "documentation/",
         schema_view.with_ui("swagger", cache_timeout=0),  # new
         name="schema-swagger-ui",
     ),
