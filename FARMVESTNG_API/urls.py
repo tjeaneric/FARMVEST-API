466cbc8e846d9511327f1fcef7dd90cb4dbcd967
JeanEric <ericjohn415@gmail.com>
2021-07-07 11:28:12 +0200

    adjusted Investment app and added swagger documentation

--- FARMVESTNG_API/urls.py
+++ FARMVESTNG_API/urls.py
@@ -18,6 +18,20 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(  # new
+    openapi.Info(
+        title="FarmvestNG API",
+        default_version="v1",
+        description="The idea is for an app that will link local farmers to investors so they can get much needed resources to expand their production. The app will work as an intermediary between investors and farmers. The app will sort for farmers, verify them and view their profiles for investors to see. The investors will have a determined rate of return for their investment.",
+        contact=openapi.Contact(email="ericjohn415@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,6 +41,16 @@ urlpatterns = [
     path("api/v1/", include("farms.urls")),
     path("api/v1/", include("Investment.urls")),
     path("api/v1/", include("Referral.urls")),
+    path(
+        "documentation",
+        schema_view.with_ui("swagger", cache_timeout=0),  # new
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),  # new
+        name="schema-redoc",
+    ),
 ]
 
 if settings.DEBUG:
