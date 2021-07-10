78102f1589502b91240edaf0d97ce0d28f3c6d94
JeanEric <ericjohn415@gmail.com>
2021-07-10 13:50:43 +0200

    refactored wallet models

--- wallet/models.py
+++ wallet/models.py
@@ -1,13 +1,7 @@
 from django.db import models
-from django.db.models.fields.related import ForeignKey
-from User.models import User
 from django.conf import settings
-from User.constants import Role
 
 
-# from .dictionary import Payment_Method
-
-# Create your models here.
 class Wallet(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     wallet_amount = models.BigIntegerField()
