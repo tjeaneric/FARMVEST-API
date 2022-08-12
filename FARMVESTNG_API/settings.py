d1dec4a534d39218e72d544efa58229149a7306e
tjeaneric <ericjohn415@gmail.com>
2022-08-12 17:59:08 +0200

    removed sendgrid credentials

--- FARMVESTNG_API/settings.py
+++ FARMVESTNG_API/settings.py
@@ -196,9 +196,3 @@ if USE_S3_STORAGE:
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
-EMAIL_HOST = "smtp.sendgrid.net"
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = config("EMAIL_PORT")
-EMAIL_USE_TLS = config("EMAIL_USE_TLS")
